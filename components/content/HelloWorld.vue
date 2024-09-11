a1070bd9e709b802a0526446398fdfbd229799a7
jinxuanzheng <jinxuanzheng@kuaishou.com>
2024-09-11 16:11:58 +0800

    feat: 新增组件

---
+++ components/content/HelloWorld.vue
@@ -0,0 +1,16 @@
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  color: {
+    type: String,
+    default: 'blue'
+  }
+})
+</script>
+
+<template>
+    <h1 :style="{color: props.color}">Hello, {{ props.title }} !!!</h1>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,17 @@
 export default defineNuxtConfig({
   extends: 'content-wind',
+
+  modules: [
+      '@nuxthq/studio'
+  ],
+
+  studio: {
+    enabled: true
+  },
+
   routeRules: {
     '/': { prerender: true }
-  }
+  },
+
+  compatibilityDate: '2024-09-11'
 })
